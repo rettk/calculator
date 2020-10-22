e179a955ad5d7c3ae2019169ff8b9a6ccc1c5a31
Rett Lam <rettlam@gmail.com>
2020-10-22 11:41:42 -0600

    first commit

---
+++ client/src/app.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+function App() {
+
+    let total = 456
+
+
+    return (
+        <div id="box">
+            <div id="calculator">
+                <form >
+                    <div id="answer-row">{total}</div>
+                </form>
+                <div id="erase"></div>
+                <div id="back"></div>
+                <div id="divide"></div>
+                <div id="7"></div>
+                <div id="8"></div>
+                <div id="9"></div>
+                <div id="multiply"></div>
+                <div id="4"></div>
+                <div id="5"></div>
+                <div id="6"></div>
+                <div id="subtract"></div>
+                <div id="1"></div>
+                <div id="2"></div>
+                <div id="3"></div>
+                <div id="add"></div>
+                <div id="0"></div>
+                <div id="switch"></div>
+                <div id="dot"></div>
+                <div id="equals"></div>
+            </div>
+
+        </div>
+    )
+}
+
+export default App
