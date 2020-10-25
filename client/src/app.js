ad0ad1dcac3eade87325b47f6e852130a31616b2
Rett Lam <rettlam@gmail.com>
2020-10-24 22:36:43 -0600

    calculator functions

--- client/src/app.js
+++ client/src/app.js
@@ -1,20 +1,72 @@
-import React from "react"
+import React, { useState } from "react"
 
 function App() {
 
-    let total = 456
+    const [numbers, setNumbers] = useState({
+        total: 0,
+        calcs: "",
+        calcsStringComplex: "3+2-(1*2)+.3",
+        calcsArrayComplex: [3, "+", 2, "-", `(`, 1, `*`, 2, `)`, "+", .3],  // should return 3.3, not 8.3
+        calcsArraySimple: [3, "+", 4, "+", 8, "-", 5] // should return 10
+    })
 
+    // function doMath(array) {
+    //     let condensedProblem = array.join("")
+    //     let condensedAnswer = eval(condensedProblem)
+    //     return console.log(condensedAnswer)
+    // }
+
+    function handleChange(e) {
+        const { name, value } = e.target
+        setNumbers(prevNumbers => ({
+            ...prevNumbers,
+            [name]: value,
+        }))
+    }
+
+    // function buttonPress(e) {
+    //     const { name, value } =e.target
+
+    // }
+
+    // Don't need this parentheses search function because of eval()
+
+    // function parenthesesSearch(array) {
+    //     let firstEnd = array.indexOf(")")
+    //     let closestStart = array.lastIndexOf("(", firstEnd)
+    //     let parenthLocations = [closestStart, firstEnd]
+    //     return console.log(parenthLocations)
+    // }
+
+
+    //Function Tests
+    // doMath(numbers.calcsArrayComplex)  WORKS
+    // console.log(eval(numbers.calcsStringComplex))  WORKS
+    // console.log(numbers.calcs)
+    // console.log(eval("5+5"))
 
     return (
         <div id="box">
             <div id="calculator">
-                <form >
-                    <div id="answer-row">{total}</div>
-                </form>
+
+                <div id="screen">
+                    <div id="calcs">
+                        <input type="text"
+                            name="calcs"
+                            value={numbers.calcs}
+                            onChange={handleChange}
+                        >
+                        </input>
+                    </div>
+                    <div id="answer-row">{eval(numbers.calcs) ? eval(numbers.calcs)
+                        : "..."}</div>
+                </div>
+
                 <div id="erase"></div>
                 <div id="back"></div>
                 <div id="divide"></div>
-                <div id="7"></div>
+                <div id="7"
+                ></div>
                 <div id="8"></div>
                 <div id="9"></div>
                 <div id="multiply"></div>
