5ce691d6ff549ee57a8bbb1c3e05e6bf57f6c7c0
Rett Lam <rettlam@gmail.com>
2020-10-26 19:44:54 -0600

    calculator 90% buttons

---
+++ client/designnotes.md
@@ -0,0 +1,21 @@
+TO DO
+- = button
+- +/- button
+- 0,+,/,* buttons can't be first text input by keyboard, but - and . can
+- show previouscalcs answer if calc incomplete
+- make it look nice
+
+ADVANCED TO DO (If Time)
+- make the operators selection actually replace the previous one instead of not working, for example pressing + and then - simply changes the + to a - instead of preventing input
+- "complex calcs off" toggle makes it so that totals are calculated at each operator press
+- add parentheses buttons
+
+
+DONE
+- after button press, return focus to input text field
+- 0,+,/,* buttons can't be first press, but - and . can
+- .,+,-,*,/ buttons can't be pressed more than once in a row, and only . can go after the others
+- 0 cannot follow an operator +,-,*,/
+
+
+
--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 function App() {
 
@@ -16,6 +16,7 @@ function App() {
     //     return console.log(condensedAnswer)
     // }
 
+
     function handleChange(e) {
         const { name, value } = e.target
         setNumbers(prevNumbers => ({
@@ -24,10 +25,44 @@ function App() {
         }))
     }
 
-    // function buttonPress(e) {
-    //     const { name, value } =e.target
+    function handleButton(e) {
+        const { value } = e.target.value
+        setNumbers(prevNumbers => ({
+            ...prevNumbers,
+            calcs: prevNumbers.calcs + e.target.value
+        }))
+        document.getElementById("field").focus()
+    }
 
-    // }
+    function doMath(string) {
+        try {
+            let answer = eval(string)
+            return answer
+        }
+        catch { console.log("error thrown") }
+    }
+
+    function equals() {
+        setNumbers(prevNumbers => ({
+            ...prevNumbers,
+            calcs: doMath(prevNumbers.calcs)
+        }))
+    }
+
+    function erase() {
+        setNumbers(prevNumbers => ({
+            ...prevNumbers,
+            calcs: ""
+        }))
+    }
+
+    function backspace() {
+        const newCalc = numbers.calcs.slice(0, -1)
+        setNumbers(prevNumbers => ({
+            ...prevNumbers,
+            calcs: newCalc
+        }))
+    }
 
     // Don't need this parentheses search function because of eval()
 
@@ -39,11 +74,13 @@ function App() {
     // }
 
 
-    //Function Tests
+    //FUNCTION TESTS console logs
     // doMath(numbers.calcsArrayComplex)  WORKS
     // console.log(eval(numbers.calcsStringComplex))  WORKS
     // console.log(numbers.calcs)
     // console.log(eval("5+5"))
+    console.log(numbers.total)
+
 
     return (
         <div id="box">
@@ -53,37 +90,164 @@ function App() {
                     <div id="calcs">
                         <input type="text"
                             name="calcs"
+                            id="field"
                             value={numbers.calcs}
                             onChange={handleChange}
+                            style={{ textAlign: "right", height: "100%", width: "96%" }}
                         >
                         </input>
                     </div>
-                    <div id="answer-row">{eval(numbers.calcs) ? eval(numbers.calcs)
+                    <div id="answer-row">{doMath(numbers.calcs) ? doMath(numbers.calcs)
                         : "..."}</div>
                 </div>
 
-                <div id="erase"></div>
-                <div id="back"></div>
-                <div id="divide"></div>
-                <div id="7"
-                ></div>
-                <div id="8"></div>
-                <div id="9"></div>
-                <div id="multiply"></div>
-                <div id="4"></div>
-                <div id="5"></div>
-                <div id="6"></div>
-                <div id="subtract"></div>
-                <div id="1"></div>
-                <div id="2"></div>
-                <div id="3"></div>
-                <div id="add"></div>
-                <div id="0"></div>
+                <div id="erase">
+                    <button
+                        onClick={erase}
+                        style={{ width: "100%", height: "100%" }}>C
+                        </button>
+                </div>
+                <div id="back">
+                    <button
+                        onClick={backspace}
+                        style={{ width: "100%", height: "100%" }}>backspace
+                        </button>
+                </div>
+                <div id="divide">
+                    <button
+                        value="/"
+                        onClick={!numbers.calcs || numbers.calcs.endsWith(".")
+                            || numbers.calcs.endsWith("+") || numbers.calcs.endsWith("-")
+                            || numbers.calcs.endsWith("/") || numbers.calcs.endsWith("*")
+                            ? null :
+                            handleButton}
+                        style={{ width: "100%", height: "100%" }}>/
+                        </button>
+                </div>
+                <div id="7">
+                    <button
+                        value="7"
+                        onClick={handleButton}
+                        style={{ width: "100%", height: "100%" }}>7
+                        </button>
+                </div>
+                <div id="8">
+                    <button
+                        value="8"
+                        onClick={handleButton}
+                        style={{ width: "100%", height: "100%" }}>8
+                        </button>
+                </div>
+                <div id="9">
+                    <button
+                        value="9"
+                        onClick={handleButton}
+                        style={{ width: "100%", height: "100%" }}>9
+                        </button>
+                </div>
+                <div id="multiply">
+                    <button
+                        value="*"
+                        onClick={!numbers.calcs || numbers.calcs.endsWith(".")
+                            || numbers.calcs.endsWith("+") || numbers.calcs.endsWith("-")
+                            || numbers.calcs.endsWith("/") || numbers.calcs.endsWith("*")
+                            ? null :
+                            handleButton}
+                        style={{ width: "100%", height: "100%" }}>*
+                        </button>
+                </div>
+                <div id="4">
+                    <button
+                        value="4"
+                        onClick={handleButton}
+                        style={{ width: "100%", height: "100%" }}>4
+                        </button>
+                </div>
+                <div id="5">
+                    <button
+                        value="5"
+                        onClick={handleButton}
+                        style={{ width: "100%", height: "100%" }}>5
+                        </button>
+                </div>
+                <div id="6">
+                    <button
+                        value="6"
+                        onClick={handleButton}
+                        style={{ width: "100%", height: "100%" }}>6
+                        </button>
+                </div>
+                <div id="subtract">
+                    <button
+                        value="-"
+                        onClick={numbers.calcs.endsWith(".")
+                            || numbers.calcs.endsWith("+") || numbers.calcs.endsWith("-")
+                            || numbers.calcs.endsWith("/") || numbers.calcs.endsWith("*")
+                            ? null :
+                            handleButton}
+                        style={{ width: "100%", height: "100%" }}>-
+                        </button>
+                </div>
+                <div id="1">
+                    <button
+                        value="1"
+                        onClick={handleButton}
+                        style={{ width: "100%", height: "100%" }}>1
+                        </button>
+                </div>
+                <div id="2">
+                    <button
+                        value="2"
+                        onClick={handleButton}
+                        style={{ width: "100%", height: "100%" }}>2
+                        </button>
+                </div>
+                <div id="3">
+                    <button
+                        value="3"
+                        onClick={handleButton}
+                        style={{ width: "100%", height: "100%" }}>3
+                        </button>
+                </div>
+                <div id="add">
+                    <button
+                        value="+"
+                        onClick={!numbers.calcs || numbers.calcs.endsWith(".")
+                            || numbers.calcs.endsWith("+") || numbers.calcs.endsWith("-")
+                            || numbers.calcs.endsWith("/") || numbers.calcs.endsWith("*")
+                            ? null :
+                            handleButton}
+                        style={{ width: "100%", height: "100%" }}>+
+                        </button>
+                </div>
+                <div id="0">
+                    <button
+                        value="0"
+                        onClick={!numbers.calcs
+                            || numbers.calcs.endsWith("+") || numbers.calcs.endsWith("-")
+                            || numbers.calcs.endsWith("/") || numbers.calcs.endsWith("*")
+                            ? null :
+                            handleButton}
+                        style={{ width: "100%", height: "100%" }}>0
+                        </button>
+                </div>
                 <div id="switch"></div>
-                <div id="dot"></div>
-                <div id="equals"></div>
+                <div id="dot">
+                    <button
+                        value="."
+                        onClick={numbers.calcs.endsWith(".")
+                            ? null :
+                            handleButton}
+                        style={{ width: "100%", height: "100%" }}>.
+                        </button>
+                </div>
+                <div id="equals">
+                    <button
+                        onClick={() => doMath(numbers.calcs) ? equals() : null}
+                        style={{ width: "100%", height: "100%" }}>=
+                        </button>
+                </div>
             </div>
-
         </div>
     )
 }
